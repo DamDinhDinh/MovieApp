4c9a98be75c8c612eed0fee4314c3fe9aa5027e4
DinhDD <dinhdd@vivas.vn>
2022-04-18 23:02:15 +0700

    Added test ViewModel

---
+++ app/src/test/java/com/example/movieapp/RxImmediateSchedulerRule.kt
@@ -0,0 +1,33 @@
+package com.example.movieapp
+
+import io.reactivex.rxjava3.android.plugins.RxAndroidPlugins
+import io.reactivex.rxjava3.plugins.RxJavaPlugins
+import io.reactivex.rxjava3.schedulers.Schedulers
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+
+/**
+ * Created by dinhdd damdinhdinh.mum@gmail.com on 4/18/2022.
+ */
+class RxImmediateSchedulerRule : TestRule {
+
+    override fun apply(base: Statement, d: Description): Statement {
+        return object : Statement() {
+            @Throws(Throwable::class)
+            override fun evaluate() {
+                RxJavaPlugins.setIoSchedulerHandler { Schedulers.trampoline() }
+                RxJavaPlugins.setComputationSchedulerHandler { Schedulers.trampoline() }
+                RxJavaPlugins.setNewThreadSchedulerHandler { Schedulers.trampoline() }
+                RxAndroidPlugins.setInitMainThreadSchedulerHandler { Schedulers.trampoline() }
+
+                try {
+                    base.evaluate()
+                } finally {
+                    RxJavaPlugins.reset()
+                    RxAndroidPlugins.reset()
+                }
+            }
+        }
+    }
+}
