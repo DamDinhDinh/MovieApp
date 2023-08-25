85932ab29a876b8c2c6d2a4767cee40a07c6b3a4
Dam Dinh <damdinhdinh.mum@gmail.com>
2023-08-25 10:32:18 +0700

    MovieListFragmentMVC

--- app/src/main/java/com/example/movieapp/presenter/movielist/MovieListFragment.kt
+++ app/src/main/java/com/example/movieapp/presenter/movielist/MovieListFragment.kt
@@ -5,35 +5,43 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.lifecycle.MutableLiveData
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.CreateMethod
 import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.common.applySchedulers
+import com.example.common.dataSchedulers
+import com.example.data.local.mapper.movie.toModel
+import com.example.data.local.roomdatabase.dao.MovieDao
+import com.example.data.remote.api.MovieApi
+import com.example.data.remote.mapper.moviepopular.toEntity
+import com.example.domain.model.movie.ModelMovie
 import com.example.movieapp.databinding.FragmentMoviePopularListBinding
 import com.example.movieapp.presenter.adapter.MovieAdapter
+import com.example.movieapp.presenter.mapper.movie.toPresent
 import com.example.movieapp.presenter.model.movie.Movie
 import dagger.hilt.android.AndroidEntryPoint
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.Disposable
 import timber.log.Timber
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class MovieListFragment : Fragment(), MovieListContract.View {
+class MovieListFragment : Fragment() {
 
     private val TAG = "MoviePopularListFragment"
 
-    private val viewModel: MovieListViewModel by viewModels()
+    @Inject
+    lateinit var movieApi: MovieApi
+
+    @Inject
+    lateinit var movieDao: MovieDao
+    private var getMoviePopularDisposable: Disposable? = null
 
     private val binding: FragmentMoviePopularListBinding by viewBinding(CreateMethod.INFLATE)
     private val movieAdapter = MovieAdapter { movie -> navigateMovieDetail(movie.id) }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        return binding.root
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -44,22 +52,51 @@ class MovieListFragment : Fragment(), MovieListContract.View {
             addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
         }
 
-        viewModel.apply {
-            observeViewState().observe(viewLifecycleOwner) { viewState -> updateViewState(viewState) }
-
-            if (observeViewState().value == null) {
-                fetchMoviePopular()
-            }
+        if ((binding.rcMoviePopular.adapter?.itemCount ?: 0) == 0) {
+            fetchMoviePopular()
         }
     }
 
-    override fun updateViewState(viewState: MovieListContract.ViewState) {
-        Timber.d("$TAG updateViewState $viewState")
-        renderMovieList(viewState.movieList)
+    private fun fetchMoviePopular() {
+        getMoviePopularDisposable?.let { if (!it.isDisposed) it.dispose() }
+        getMoviePopularDisposable =
+            getPopular().applySchedulers()
+                .map { list -> list.map { it.toPresent() } }
+                .subscribe(
+                    { list ->
+                        movieAdapter.submitList(list)
+                    }, { error -> error.printStackTrace() }
+                )
     }
 
-    private fun renderMovieList(movieList: List<Movie>) {
-        movieAdapter.submitList(movieList)
+    private fun getPopular(): Observable<List<ModelMovie>> {
+        fetchAndSavePopularMovie()
+        return movieDao.getPopularMovie()
+            .map { list -> list.map { it.toModel() } }
+    }
+
+    private fun fetchAndSavePopularMovie() {
+        movieApi.getPopular()
+            .dataSchedulers()
+            .map { it.results?.map { movie -> movie.toEntity() } ?: listOf() }
+            .subscribe({ list -> movieDao.insert(list) },
+                { error -> error.printStackTrace()})
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        getMoviePopularDisposable?.let {
+            if (!it.isDisposed) it.dispose()
+        }
     }
 
     private fun navigateMovieDetail(id: Int) {
